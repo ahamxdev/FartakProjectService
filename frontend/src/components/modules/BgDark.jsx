ede0c89fe2ab412635fd3b87ce55fac0415bba00
AmirHossein AliMohammadi <ahamdev.programmer@gmail.com>
2025-05-18 23:48:03 +0330

    Deleting the frontend code except for the dashboard

--- frontend/src/components/modules/BgDark.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BgDark = ({ setIsSomeThingOpen }) => {
-      return (
-            <div onClick={() => setIsSomeThingOpen(false)} className='w-full cursor-pointer h-screen fixed top-0 left-0 z-20 bg-black opacity-50'></div>
-      )
-}
-
-export default BgDark
