0bace70572998b0685773e836ffbb2f900fd20ea
SalehRezaei2002 <amsaleh.r81@gmail.com>
2025-07-10 18:45:51 +0330

    handle adding Authorization

--- frontend/next/src/utils/api.ts
+++ frontend/next/src/utils/api.ts
@@ -9,17 +9,41 @@
 //   return res.json();
 // };
 
+// export const api = async (
+//   url: string,
+//   method: string = "GET",
+//   body?: unknown
+// ) => {
+//   const res = await fetch(`https://api.fartakproject.ir${url}`, {
+//     method,
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: body ? JSON.stringify(body) : undefined,
+//   });
+//   return res;
+// };
+
 export const api = async (
   url: string,
   method: string = "GET",
   body?: unknown
 ) => {
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+  };
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
   const res = await fetch(`https://api.fartakproject.ir${url}`, {
     method,
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers,
     body: body ? JSON.stringify(body) : undefined,
   });
-  return res.json();
+
+  return res;
 };
