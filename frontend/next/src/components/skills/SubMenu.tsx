aba20f0be78bce77863d1e37b155f4c11c7f2081
SalehRezaei2002 <amsaleh.r81@gmail.com>
2025-07-10 18:46:00 +0330

    get new inputs

--- frontend/next/src/components/skills/SubMenu.tsx
+++ frontend/next/src/components/skills/SubMenu.tsx
@@ -1,32 +1,47 @@
 import React from "react";
 
+type ItemType = {
+  projectTypeId: number;
+  title: string;
+};
 const SubMenu = ({
   items,
   isSubMenuOpen,
+  setProjectSkillId,
+  setIsSubMenuOpen,
+  setProjectSkillTitle,
 }: {
-  items: string[];
+  items: ItemType[];
   isSubMenuOpen: boolean;
+  setProjectSkillId: (value: number) => void;
+  setIsSubMenuOpen: (value: boolean) => void;
+  setProjectSkillTitle: (value: string) => void;
 }) => {
-  console.log(items);
-  
   return (
     <>
       <ul
-        className={`absolute top-[105%] z-50 left-2 right-2 bg-white rounded-sm transition-all duration-100 ${
+        className={`absolute top-[105%] z-50 left-2 right-2 flex flex-col gap-2 bg-white rounded-sm transition-all duration-100 ${
           isSubMenuOpen ? "h-auto py-1 px-4" : "h-0 py-0 px-0"
         }`}
         id="category-submenu"
       >
-        <li
-          className={`text-sm cursor-pointer hover:bg-zinc-200 rounded-md transition-all duration-75 hover:text-white py-0.5 px-2 font-bold text-[#000] ${
-            isSubMenuOpen ? "block" : "hidden"
-          } `}
-        >
-          ترجمه
-        </li>
+        {items?.map((item) => (
+          <li
+            className={`text-sm cursor-pointer hover:bg-zinc-600 rounded-md transition-all duration-75 hover:text-white py-2 px-2 font-bold text-[#000] ${
+              isSubMenuOpen ? "block" : "hidden"
+            } `}
+            key={item.projectTypeId}
+            onClick={() => {
+              setProjectSkillId(item.projectTypeId);
+              setIsSubMenuOpen(false);
+              setProjectSkillTitle(item.title);
+            }}
+          >
+            {item.title}
+          </li>
+        ))}
       </ul>
     </>
   );
 };
-
 export default SubMenu;
