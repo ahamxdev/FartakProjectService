80812cf37714b74bf72e0a60e1004e05c18c8bfb
SalehRezaei2002 <amsaleh.r81@gmail.com>
2025-07-10 18:46:35 +0330

    handle adding border for selected item

--- frontend/next/src/components/skills/WaySpeedProjectRegistrationBox.tsx
+++ frontend/next/src/components/skills/WaySpeedProjectRegistrationBox.tsx
@@ -4,16 +4,25 @@ import React from "react";
 const WaySpeedProjectRegistrationBox = ({
   title,
   factor,
+  onHandle,
+  priority,
 }: {
   title?: string;
-  factor: string;
+  factor?: string;
+  onHandle: () => void;
+  priority?: string;
 }) => {
   return (
     <div
-      className="py-6 px-6 cursor-pointer rounded-[20px] flex flex-col items-center gap-5"
+      className={`py-6 px-6 cursor-pointer rounded-[20px] flex flex-col items-center gap-5 ${
+        priority === title ? "border border-blue-700" : ""
+      } `}
       id="waySpeed-project-registration-box"
+      onClick={onHandle}
     >
-      <span className="md:text-xl text-center text-sm font-medium text-[#000] w-full">{title}</span>
+      <span className="md:text-xl text-center text-sm font-medium text-[#000] w-full">
+        {title}
+      </span>
       <div className="relative lg:w-[140px] w-[70px] lg:h-[130px] h-[60px]">
         <Image
           src={"/images/approved.png"}
