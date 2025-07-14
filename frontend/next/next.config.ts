65123bfbd0b77759081bab6ffcc15d629afa69b1
SalehRezaei2002 <amsaleh.r81@gmail.com>
2025-07-14 15:33:54 +0330

    add domain

--- frontend/next/next.config.ts
+++ frontend/next/next.config.ts
@@ -1,7 +1,19 @@
+// import { NextConfig } from "next";
+// import createNextIntlPlugin from "next-intl/plugin";
+
+// const nextConfig: NextConfig = {};
+
+// const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+// export default withNextIntl(nextConfig);
+
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["api.fartakproject.ir" , "https://www.fartakproject.ir"],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 export default withNextIntl(nextConfig);
