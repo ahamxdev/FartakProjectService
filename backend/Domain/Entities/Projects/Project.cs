ae052c5b220b76dd7e3926c065668a7483645b77
ملیکا امیری <malihe_ghafouri@yahoo.com>
2025-07-14 13:27:06 +0330

    ProjectTeamHead and  Portfolio

--- backend/Domain/Entities/Projects/Project.cs
+++ backend/Domain/Entities/Projects/Project.cs
@@ -15,8 +15,11 @@
         public required string CreateDate {  get; set; }
         public required string DeadLine {  get; set; }
         public int Status { get; set; } // 0 Nothing 1 Doing 2 Finish 
+        public int StarRating { get; set; } // 0 - 5 StarRating 
         public int Priority { get; set; } // 0 Price 2 Quality 2 Both
         public int PriceType { get; set; } // 0 Simple 1 Medium  2 Large 
         public string? Image { get; set; }
+        public string? PortfolioImage { get; set; } // تصویر نمونه کار 
+        public string? PortfolioDescription { get; set; } // توضیحات نمونه کار 
     }
 }
