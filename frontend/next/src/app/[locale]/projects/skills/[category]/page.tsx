11dc9f2b58085f5c401195647b5546d77a40a498
SalehRezaei2002 <amsaleh.r81@gmail.com>
2025-07-16 16:18:43 +0330

    get and show detail of each skill box

--- frontend/next/src/app/[locale]/projects/skills/[category]/page.tsx
+++ frontend/next/src/app/[locale]/projects/skills/[category]/page.tsx
@@ -1,24 +1,64 @@
-"use client"
-import React from "react"
-import BreadCrumb from "@/components/skills/BreadCrumb"
-import OnlineStoreDesign from "@/components/skills/OnlineStoreDesign"
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import React, { useEffect, useState } from "react";
+import BreadCrumb from "@/components/skills/BreadCrumb";
+import OnlineStoreDesign from "@/components/skills/OnlineStoreDesign";
+import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { api } from "@/utils/api";
 // import ContentCreation from "@/components/skills/ContentCreation"
 // import ApplicationWebsiteDesign from "@/components/skills/ApplicationWebsiteDesign"
 // import Programming from "@/components/skills/Programming"
 // import Translation from "@/components/skills/Translation"
 // import WebDesign from "@/components/skills/WebDesign"
 
+type skillInfo = {
+  description: string;
+  image: string;
+  projectSkillId: number;
+  projectSkillParentId: number;
+  title: string;
+};
+
 const CategoryInfo = () => {
+  const params = useParams<{ category: string }>();
+  const id = params.category;
+  console.log(id);
+
+  const [skillInfo, setSkillInfo] = useState<skillInfo[]>([]);
+
+  const getCtgs = {
+    projectSkillId: id.split("-")[0],
+  };
+  useEffect(() => {
+    api("/api/ProjectSkills/GetById", "POST", getCtgs)
+      .then((res) => {
+        if (res.status == 200) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setSkillInfo(data.projectSkills);
+      });
+  }, []);
   return (
     <>
-
-      <BreadCrumb items={["مهارت ها شما", "طراحی سایت و اپلیکیشن", "طراحی رابط کاربری"]} />
-      <section className='flex flex-col lg:flex-row lg:gap-16 gap-8 w-[90%] mx-auto md:my-10 my-5'>
+      <BreadCrumb
+        items={[
+          "مهارت ها شما",
+          "طراحی سایت و اپلیکیشن",
+          decodeURIComponent(id.split("-")[1]),
+        ]}
+      />
+      <section className="flex flex-col lg:flex-row lg:gap-16 gap-8 w-[90%] mx-auto md:my-10 my-5">
         <div className="relative lg:w-[55%] w-full lg:h-[530px] h-[350px]">
           <Image
-            src="/skills/image 10.png"
+            src={`${
+              skillInfo[0]?.image
+                ? `https://api.fartakproject.ir/upload/Projects/${skillInfo[0].image}`
+                : "/images/image 10.png"
+            }`}
             alt="category-detail-image"
             fill
             sizes="(max-width: 768px) 100vw, 600px"
@@ -26,18 +66,21 @@ const CategoryInfo = () => {
           />
         </div>
 
-
         <div className="flex flex-col justify-between lg:w-[45%] w-full">
           <div className="flex flex-col gap-5">
-            <h1 className='text-[#1450A3] font-bold text-lg md:text-xl lg:text-[32px]'>طراحی رابط کاربری</h1>
-            <p className='font-normal leading-6 text-sm md:text-base lg:text-xl text-justify'>
-              سفارش این پروژه نقطهٔ شروع تبدیل ایده‌های دیجیتال شما به یک واقعیت ملموس است.
-              در این پروژه، با رویکردی نوین و استاندارد، ساختاری منسجم و ظاهری جذاب برای وب‌سایت شما طراحی می‌شود.
-              تمامی جنبه‌ها از طراحی واکنش‌گرا و بهینه‌سازی سرعت گرفته تا هماهنگی دقیق المان‌های بصری و فنی مد نظر قرار گرفته تا سایت شما در انواع دستگاه‌ها به بهترین شکل نمایش داده شود.
-              تیم مجرب ما با دقت به جزئیات و بهره‌گیری از تجربه‌های عملی، محیطی حرفه‌ای و مدرن را برای حضور شما در فضای آنلاین فراهم می‌آورد.
+            <h1 className="text-[#1450A3] font-bold text-lg md:text-xl lg:text-[32px]">
+              {decodeURIComponent(id.split("-")[1])}
+            </h1>
+            <p className="font-normal leading-6 text-sm md:text-base lg:text-xl text-justify">
+              {skillInfo[0]?.description}
             </p>
           </div>
-          <Link href={"/projects/project-registration"} className='bg-[#1D40D7] text-white rounded-lg flex justify-center items-center md:text-lg text-xs font-bold w-full py-2 my-6 lg:my-0'>همین الان پروژت رو ثبت کن</Link>
+          <Link
+            href={"/projects/project-registration"}
+            className="bg-[#1D40D7] text-white rounded-lg flex justify-center items-center md:text-lg text-xs font-bold w-full py-2 my-6 lg:my-0"
+          >
+            همین الان پروژت رو ثبت کن
+          </Link>
         </div>
       </section>
 
@@ -52,9 +95,8 @@ const CategoryInfo = () => {
       {/* <Translation /> */}
 
       {/* <WebDesign /> */}
-
     </>
-  )
-}
+  );
+};
 
-export default CategoryInfo
+export default CategoryInfo;
