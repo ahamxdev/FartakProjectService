cde6edbb6345f87017f49a1b876d0f9d71c581f0
SalehRezaei2002 <amsaleh.r81@gmail.com>
2025-07-19 22:02:34 +0330

    change to pre version

--- frontend/next/src/utils/api.ts
+++ frontend/next/src/utils/api.ts
@@ -47,3 +47,43 @@ export const api = async (
 
   return res;
 };
+
+// export const api = async (
+//   url: string,
+//   method: string = "GET",
+//   body?: unknown,
+  
+// ) => {
+//   const token =
+//   typeof window !== "undefined" ? localStorage.getItem("token") : null;
+//   const userId =
+//   typeof window !== "undefined" ? localStorage.getItem("userId") : null;
+
+  
+
+
+//   const isFormData =
+//     typeof FormData !== "undefined" && body instanceof FormData;
+
+//   const headers: HeadersInit = {};
+
+//   if (!isFormData) {
+//     headers["Content-Type"] = "application/json";
+//   }
+
+//   if (token) {
+//     headers["Authorization"] = `Bearer ${token}-${userId}`;
+//   }
+
+//   const res = await fetch(`https://api.fartakproject.ir${url}`, {
+//     method,
+//     headers,
+//     body: body
+//       ? isFormData
+//         ? (body as FormData)
+//         : JSON.stringify(body)
+//       : undefined,
+//   });
+
+//   return res;
+// };
